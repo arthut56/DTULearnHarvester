d890651d5d28647db3cc349da086bc309097f60a
arthut <arturocortes2004@gmail.com>
2026-04-21 15:34:12 +0200

    added enum to format options

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from enum import Enum
+
 import typer
 
-from script import download_course
+from script import download_course, FORMAT_MAPPING
 from typing import Annotated
 
+FormatStyle = Enum("FormatStyle", {k: k for k in FORMAT_MAPPING})
 
-def main(course_no, format_style: Annotated[str, typer.Argument()] = ""):
-    download_course(course_no, format_style)
+def main(course_no, format_style: Annotated[FormatStyle, typer.Argument()] = ''):
+    download_course(course_no, format_style.value)
 
 
 if __name__ == "__main__":
